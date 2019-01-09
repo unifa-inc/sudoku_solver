571312ebd35465b2852242e5b932937ba9c9ce07
Shizuka Kakimoto <shizuka.kakimoto@unifa-e.com>
2019-01-09 12:55:28 +0900

    refactor and some bug fix

---
+++ setup.js
@@ -0,0 +1,17 @@
+$(window).on("load", () => {
+  /** グリッド描画 ブロックごとに色分け */
+  let trs = "";
+  for (let i = 0; i < 9; i++) {
+    let tds = "";
+    for (let j = 0; j < 9; j++) {
+      let cls = (Math.floor(i/3) + Math.floor(j/3))%2 < 1 ? "filled" : "";
+      tds += `
+        <td>
+          <input id="cell_${i}_${j}" class="cell ${cls}" type="text" maxlength="1">
+        </td>
+      `;
+    }
+    trs += "<tr>" + tds + "</tr>";
+  }
+  $("table#grid")[0].innerHTML = trs;
+});
--- solver.js
+++ solver.js
@@ -1,21 +1,3 @@
-$(window).on("load", () => {
-  /** グリッド描画 ブロックごとに色分け */
-  let trs = "";
-  for (let i = 0; i < 9; i++) {
-    let tds = "";
-    for (let j = 0; j < 9; j++) {
-      let cls = (Math.floor(i/3) + Math.floor(j/3))%2 < 1 ? "filled" : "";
-      tds += `
-        <td>
-          <input id="cell_${i}_${j}" class="cell ${cls}" type="text" maxlength="1">
-        </td>
-      `;
-    }
-    trs += "<tr>" + tds + "</tr>";
-  }
-  $("table#grid")[0].innerHTML = trs;
-});
-
 $("#solve").on("click", () => {
   /** vals（２次元配列）に現在の入力値を保持 */
   let vals = [];
@@ -28,23 +10,32 @@ $("#solve").on("click", () => {
   }
   console.log(vals);
 
-  /** 1~9の各数字を左上からブロックごとに算定してvalsに保持 */
-  for (let n = 1; n <= 1; n++) {
-    for (let b = 0; b < 9; b++) {
-      fillBlock(n, b, vals);
+  /** 答えが見つからなくなるまでループ */
+  let solved_cnt;
+  do {
+    solved_cnt = 0;
+    /** 1~9の各数字を左上からブロックごとに算定してvalsに保持 */
+    for (let n = 1; n <= 9; n++) {
+      for (let b = 0; b < 9; b++) {
+        console.log(`Block: ${b}, n: ${n}`);
+        const solved = solveBlock(n, b, vals);
+        solved_cnt += solved ? 1 : 0;
+      }
     }
-  }
+  } while (solved_cnt > 0);
 
   /** valsの値をグリッドに描画 */
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      $(`#cell_${i}_${j}`).val(vals[i][j] > 0 ? vals[i][j] : "");
-      // TODO: 自動算出した値は色を変える
+      if (vals[i][j] > 0 && $(`#cell_${i}_${j}`).val() != vals[i][j]) {
+        $(`#cell_${i}_${j}`).val(vals[i][j]);
+        $(`#cell_${i}_${j}`).addClass('solved');
+      }
     }
   }
 });
 
-const fillBlock = (val, block, vals) => {
+const solveBlock = (val, block, vals) => {
   /** 各ブロックの左上のセル位置 */
   const block_origins = [[0,0],[0,3],[0,6],[3,0],[3,3],[3,6],[6,0],[6,3],[6,6]];
 
@@ -66,7 +57,7 @@ const fillBlock = (val, block, vals) => {
   console.log(block_vals);
 
   for (let i = 0; i < 3; i++) {
-    if (vals[org_r+i].indexOf(val) > 0) {
+    if (vals[org_r+i].indexOf(val) >= 0) {
       for (let j = 0; j < 3; j++) {
         block_vals[i][j] = 0;
       }
@@ -102,4 +93,6 @@ const fillBlock = (val, block, vals) => {
     }
   }
   console.log(vals);
+
+  return sum === 1 ? true : false;
 };
